fc989d5402c0967a26f9655db3b7be38a8f84eef
riskaaaac <148666879+riskaaaac@users.noreply.github.com>
2023-11-07 13:21:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Login() {
     var done = 0;
     var username = document.login.username.value;
     var password = document.login.password.value;
-    if (username == "Calvin" && password =="William"){
+    if (username == "Riska" && password =="Alfianwar"){
             if(confirm("Apakah anda ingin login?") == true){
                 window.location.href = "https://github.com/riskaaaac"
             } else {
