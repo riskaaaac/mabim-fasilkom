e76f016cdd0da924ac8c7c327ce8c207a51c29b1
riskaaaac <148666879+riskaaaac@users.noreply.github.com>
2023-11-07 13:13:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function Login() {
+    var done = 0;
+    var username = document.login.username.value;
+    var password = document.login.password.value;
+    if (username == "Calvin" && password =="William"){
+            if(confirm("Apakah anda ingin login?") == true){
+                window.location.href = "https://github.com/calvinww123"
+            } else {
+                alert("Login dibatalkan !");
+            }
+            
+    } else{
+            if (done == 0) {
+                alert("Username/Pasword Salah");
+            }
+    }
+} 
